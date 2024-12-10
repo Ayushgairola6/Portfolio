b02d544d9d305ba5581a891c914b4e24cdc2745c
ayush <ayushgairola2002@gmail.com>
2024-12-10 12:59:37 +0530

    optimized version3

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -3,24 +3,20 @@ import { FaXTwitter } from 'react-icons/fa6'
 
 const Footer = () => {
 
-    function handleMail(){
-        if(typeof window !== undefined){
-            window.location.href="mailto:ayushgairola2002@gmail.com?subject=Hello&body=Hi i want to collaborate with you.";
-         }
-      }
+    
 
     return <>
         <footer className='py-10 bg-white/5  flex items-center justify-evenly relative'>
             <div className='absolute right-24 top-4   inline-flex gap-4'>
-                <FaWhatsapp onClick={window.open('https://wa.me/918126687562')} />
-                <FaXTwitter onClick={window.open('https://x.com/AyushGairo14362')} />
-                <FaInstagram onClick={window.open('')} />
+                <FaWhatsapp href="https://wa.me/918126687562"/>
+                <FaXTwitter href="https://x.com/AyushGairo14362" />
+                <FaInstagram  />
             </div>
             <h6 className="font-cosiTimes font-bold text-3xl"> <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">@Ayush</span></h6>
             <div className='flex  items-center justify-between py-2 px-12 w-72'>
                 <a className='hover:underline' href="">About</a>
                 <a className='hover:underline' href="">Services</a>
-                <a className='hover:underline' href="" onClick={handleMail}>Contact</a>
+                <a className='hover:underline' href="mailto:ayushgairola2002@gmail.com?subject=Hello&body=Hi i want to collaborate with you." >Contact</a>
             </div>
 
 
